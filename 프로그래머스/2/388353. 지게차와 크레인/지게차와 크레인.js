b00d56a81389828aa3f62d3581552492769b76af
Mooni <88727410+moon0727@users.noreply.github.com>
2025-03-16 21:16:20 +0900

    [level 2] Title: 지게차와 크레인, Time: 80.61 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.js
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.js
@@ -85,62 +85,81 @@
     
 //     return countContainers();
 // }
-
 function solution(storage, requests) {
     var answer = 0;
-    let n = storage.length;
-    let m = storage[0].length
-    let arr = Array.from({length:1},()=>Array.from({length:m+2},()=>''));
-    for(let i =0; i < storage.length; i ++){
-        let a = ['', ...storage[i], ''];
-        arr.push(a)
+    
+    let n = storage.length;  //행개수
+    let m = storage[0].length;  //열개수
+    
+    //storage 위 아래 양 옆이 ''으로 채워진 배열 arr 생성
+    let arr = Array.from({ length: n + 2 }, () => Array(m + 2).fill(''));
+    
+    //기존 배열을 가운데에 복사
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            arr[i + 1][j + 1] = storage[i][j];
+        }
     }
-    arr.push(Array.from({length:m+2},()=>''));
-
+    
+    //글자 하나씩 bfs
     for(let str of requests){
-        bfs(arr,str,n+2,m+2)
+        bfs(arr, str, n + 2, m + 2)
     }
 
+		//arr에 남은 알파벳 개수 출력
     for(let a of arr){
         answer += a.filter(v => v !== '').length;
     }
     return answer;
 }
 
+//위 아래 왼쪽 오른쪽
 const dx = [0,0,-1,1];
 const dy = [-1,1,0,0]
 
-function bfs(arr,str,n,m){
-    let visit = Array.from({length:n},()=>Array.from({length:m},()=>false));
-    visit[0][0] = true
-    let q= [[0,0]];
 
-    let his = {}
+function bfs(arr, str, n, m){
+		//방문한 컨테이너 확인용 array, false로 초기화
+    let visit = Array.from({length : n}, () => Array.from({length : m}, () => false));
+    
+    visit[0][0] = true  //첫번째 노드 방문
+    
+    let queue = [[0,0]];  //queue에 넣기
 
-    while(q.length > 0){
-        const [x,y] = q.pop();
+    let history = {}
 
-        for(let i=0; i < 4; i++){
+    while(queue.length > 0){  //큐에 남은 노드(컨테이너)가 있다면
+        const [x,y] = queue.pop();  //노드 빼기
+
+        for(let i = 0; i < 4; i++){  //4방향 확인
             const nx = x + dx[i];
             const ny = y + dy[i];
-
+           
+						//존재하는 index + 방문한 적 없는 노드
             if(0 <= nx && nx < n && 0 <= ny && ny < m && visit[nx][ny] === false){
+                //request가 컨테이너의 알파벳과 동일하다면
                 if(str[0] === arr[nx][ny]){
-                    his[`${nx} ${ny}`] = 1
-                    // his.set(`${nx} ${ny}`,1)
+		                //문자열 키 생성
+                    history[`${nx} ${ny}`] = 1
                 }
+                
+                //str의 길이가 1이고(bfs 탐색이 필요함) 현재 노드가 빈 문자열을 가졌을 경우
                 if(str.length === 1 && arr[nx][ny] === ''){
+		                //방문 true
                     visit[nx][ny] = true;
-                    q.push([nx,ny])
-                }else if(str.length === 2){
+                    queue.push([nx,ny])  //queue에 넣기
+                } else if(str.length === 2){  //str의 길이가 2이면
+                    //방문 true
                     visit[nx][ny] = true;
-                    q.push([nx,ny])
+                    queue.push([nx,ny])  //queue에 넣기
                 }
             }
         }
     }
-    for(let key in his){
-        const [x,y] = key.split(' ').map(Number);
+    
+    //저장해두었던 키 분해해서 알파벳 빼기
+    for(let key in history) {
+        const [x, y] = key.split(' ').map(Number);
         arr[x][y] = ''
     }
 }
